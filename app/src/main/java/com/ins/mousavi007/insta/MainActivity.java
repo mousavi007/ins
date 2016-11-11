c4abb3cc8631077450cb88b5243a5e930b8532cf
darya <mousavi_1986@yahoo.com>
2016-11-11 17:13:21 +0330

    Initial commit

---
+++ app/src/main/java/com/ins/mousavi007/insta/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ins.mousavi007.insta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
